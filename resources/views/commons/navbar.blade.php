2a0616b39d65e886eecad91ea72fe230c8785c00
norapandaex <good.boys@live.jp>
2021-04-08 12:26:09 +0000

    user pages

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,12 +12,12 @@
             <ul class="navbar-nav">
                 @if (Auth::check())
                 {{-- ユーザー一覧ページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
+                <li class="nav-item">{!! link_to_route('users.index', 'users', [], ['class' => 'nav-link']) !!}</li>
                 <li class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                     <ul class="dropdown-menu dropdown-menu-right">
                         {{-- ユーザー詳細ページへのリンク --}}
-                        <li class="dropdown-item"><a href="#">My profile</a></li>
+                        <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['user' => Auth::id()]) !!}</li>
                         <li class="dropdown-divider"></li>
                         {{-- ログアウトへのリンク --}}
                         <li class="dropdown-item">{!! link_to_route('logout.get', 'logout') !!}</li>
@@ -27,7 +27,7 @@
                 {{-- ユーザー登録ページのリンク --}}
                 <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
                 {{-- ログインページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
                 @endif
             </ul>
         </div>
---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section
+    <div class="row">
+        <aside class="col-sm-4">
+            <div class="card-header">
+                <h3 class="card-title">{{ $user->name }}</h3>
+            </div>
+            <div class="card-body">
+                {{-- ユーザーのメールアドレスをもとにGravatarを取得して表示 --}}
+                <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
+            </div>
+        </aside>
+        <div class="col-sm-8">
+            <ul class="nav nav-tabs nav-justified mb-3">
+                {{-- ユーザ詳細タブ --}}
+                <li class="nav-item"><a href="#" class="nav-link">TimeLine</a></li>
+                {{-- フォロー一覧タブ --}}
+                <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
+                {{-- フォロワー一覧タブ --}}
+                <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
+            </ul>
+        </div>
+    </div>
+@endsection
