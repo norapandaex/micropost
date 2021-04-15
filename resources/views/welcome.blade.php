ef2c2bcd3505c19be1698d8d9769bf7a7a2c38cd
norapandaex <good.boys@live.jp>
2021-04-15 05:24:34 +0000

    timeline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    @if(Auth::check())
+    @if (Auth::check())
         <div class="row">
             <aside class="col-sm-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ Auth::user() -> name }}</h3>
-                    </div>
-                    <div>
-                        {{-- ユーザーのメールアドレスをもとにGravatearを取得して表示 --}}
-                        <img class="rounded img-fluid" src="{{ Gravatar::get(Auth::user()->email, ['size' => 500]) }}" alt="">
-                    </div>
-                </div>
+                {{-- ユーザ情報 --}}
+                @include('users.card')
             </aside>
             <div class="col-sm-8">
                 {{-- 投稿フォーム --}}
@@ -25,7 +18,7 @@
         <div class="center jumbotron">
             <div class="text-center">
                 <h1>Welcome to the Microposts</h1>
-                {{-- ユーザー登録ページへのリンク --}}
+                {{-- ユーザ登録ページへのリンク --}}
                 {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
